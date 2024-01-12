2114bb53b1ff575c0378ee18d0d47f843f37dc6a
abbosnasriddinov6 <abbosbeknasriddinov001@gmail.com>
2024-01-12 21:41:27 +0500

    gallery

--- src/components/apiposts/Apiposts.jsx
+++ src/components/apiposts/Apiposts.jsx
@@ -4,6 +4,7 @@ const Apiposts = () => {
   const [users, setUser] = useState([]);
   const [posts, setPosts] = useState([]);
   const [todos, setTodos] = useState([]);
+  const [gallery, setGalllery] = useState([])
 
   const fetchUser = async () => {
     try {
@@ -37,6 +38,16 @@ const Apiposts = () => {
     }
   };
 
+  const fetchGallery = async (id) => {
+    try {
+      const str = await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${id.target.id}`);
+      const st = await str.json();
+      setGalllery(st);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     fetchUser();
   }, []);
@@ -51,6 +62,7 @@ const Apiposts = () => {
             <li>{users.name}</li>
             <li>{users.username}</li>
             <li>{users.email}</li>
+            <div className="buttons">
             <button 
             id={users.id} 
             onClick={(e) => fetchItems(e)}
@@ -65,6 +77,14 @@ const Apiposts = () => {
             >
               Todos
             </button>
+            <button
+              id={users.id}
+              className="todos"
+              onClick={(e) => fetchGallery(e)}
+            >
+              Albums
+            </button>
+            </div>
           </ul>
         ))}
       </div>
@@ -89,6 +109,16 @@ const Apiposts = () => {
                   <li>{todos?.id}</li>
                   <li>{todos?.title}</li>
                   <li>{todos?.completed}</li>
+                </ul>;
+              </>)
+            : ""}
+        </div>
+        <div className="posts">
+          {gallery?.length
+            ? gallery.map((albums) => <>
+                <ul className="todoss" key={albums.userId}>
+                  <li>{albums?.id}</li>
+                  <li>{albums?.title}</li>
                 </ul>;
               </>)
             : ""}
